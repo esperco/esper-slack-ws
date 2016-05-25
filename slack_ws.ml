1ea9b4943d311d25a180ec516bef0e9dcde1dbe3
Martin Jambon <martin@esper.com>
2016-05-24 18:50:26 -0700

    Work in progress

--- slack_ws.ml
+++ slack_ws.ml
@@ -1,4 +1,22 @@
 open Lwt
 
+(*
+   - open websocket
+   - post to elb
+   - save timestamp of latest event
+   - update socket connection
+*)
+
+let connections = Hashtbl.create 10
+
+let create_connection slack_teamid =
+  Slack.get_auth slack_teamid >>= function
+  | None ->
+      logf `Error "Cannot connect to Slack for team %s"
+        (Slack_api_teamid.to_string slack_teamid);
+      return ()
+  | Some auth ->
+      ...
+
 let init () =
   return ()
