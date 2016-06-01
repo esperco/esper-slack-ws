807ff6e05a974824dfdfd21c42329a669ff7c00b
Martin Jambon <martin@esper.com>
2016-05-31 19:05:09 -0700

    Work in progress

--- slack_ws.ml
+++ slack_ws.ml
@@ -9,11 +9,32 @@ open Log
    - update socket connection
 *)
 
-(*
-let connections = Hashtbl.create 10
-*)
+type connection = {
+  conn_id: Slack_api_teamid.t;
+  push: string -> unit Lwt.t;
+  close: unit -> unit Lwt.t;
+  mutable waiting_for_pong: (bool Lwt.t * unit Lwt.u) option ref;
+}
 
-let react input_handler send frame =
+let connections = Hashtbl.create 10
+
+let remove_connection id =
+  try
+    let {close} = Hashtbl.find connections id in
+    Hashtbl.remove connections id;
+    catch close
+      (fun e ->
+         logf `Error "Can't close websocket connection: %s" (string_of_exn e);
+         return ()
+      )
+  with Not_found ->
+    return ()
+
+let replace_connection x =
+  async (fun () -> remove_connection x.conn_id);
+  Hashtbl.add connections x.conn_id x
+
+let react input_handler waiting_for_pong send frame =
   let open Websocket_lwt.Frame in
   match frame.opcode with
   | Opcode.Ping ->
@@ -21,7 +42,6 @@ let react input_handler send frame =
       return true
 
   | Opcode.Close ->
-      (* Immediately echo and pass this last message to the user *)
       (if String.length frame.content >= 2 then
          send (
            Websocket_lwt.Frame.create ~opcode:Opcode.Close
@@ -33,6 +53,12 @@ let react input_handler send frame =
       return false
 
   | Opcode.Pong ->
+      (match !waiting_for_pong with
+       | None -> ()
+       | Some (_, awakener) ->
+           waiting_for_pong := None;
+           Lwt.wakeup awakener ()
+      );
       return true
 
   | Opcode.Text
@@ -46,7 +72,7 @@ let react input_handler send frame =
       send (Websocket_lwt.Frame.close 1002) >>= fun () ->
       return false
 
-let create_websocket_connection ws_url input_handler =
+let create_websocket_connection ws_url input_handler waiting_for_pong =
   let open Websocket_lwt.Frame in
   let uri = Uri.of_string ws_url in
   Resolver_lwt.resolve_uri ~uri Resolver_lwt_unix.system >>= fun endp ->
@@ -56,7 +82,7 @@ let create_websocket_connection ws_url input_handler =
 
   let rec loop () =
     recv () >>= fun frame ->
-    react input_handler send frame >>= function
+    react input_handler waiting_for_pong send frame >>= function
     | true -> loop ()
     | false -> return ()
   in
@@ -75,12 +101,67 @@ let create_connection slack_teamid input_handler =
   | None ->
       logf `Error "Cannot connect to Slack for team %s"
         (Slack_api_teamid.to_string slack_teamid);
-      return ()
+      Http_exn.bad_request
+        `Slack_authentication_missing
+        "Slack authentication missing"
   | Some auth ->
       Slack_api.rtm_start auth.Slack_api_t.access_token >>= fun x ->
       let ws_url = x.Slack_api_t.url in
-      create_websocket_connection ws_url input_handler >>= fun (push, close) ->
-      return ()
+      let waiting_for_pong = ref None in
+      create_websocket_connection ws_url
+        input_handler waiting_for_pong >>= fun (push, close) ->
+      let conn = {
+        conn_id = slack_teamid;
+        push;
+        close;
+        waiting_for_pong;
+      } in
+      return conn
+
+let get_connection slack_teamid input_handler =
+  let mutex_key = "slack-ws:" ^ Slack_api_teamid.to_string slack_teamid in
+  Redis_mutex.with_mutex ~atime:30. ~ltime:60 mutex_key (fun () ->
+    let opt =
+      try Some (Hashtbl.find connections slack_teamid)
+      with Not_found -> None
+    in
+    match opt with
+    | Some x -> return x
+    | None ->
+        create_connection slack_teamid input_handler >>= fun conn ->
+        replace_connection conn;
+        return conn
+  )
+
+(*
+   Send a ping and wait for a pong response for up to 10 seconds.
+*)
+let test_connection x =
+  match !(x.waiting_for_pong) with
+  | Some (result, _) ->
+      result
+  | None ->
+      Apputil_error.catch_and_report "Slack Wwebsocket ping"
+        (fun () ->
+           let waiter, awakener = Lwt.wait () in
+           let success = waiter >>= fun () -> return true in
+           let failure = Lwt_unix.sleep 10. >>= fun () -> return false in
+           let result = Lwt.pick [success; failure] in
+           x.waiting_for_pong := Some (result, awakener);
+           result
+        )
+        (fun e ->
+           logf `Error "Websocket ping error: %s" (string_of_exn e);
+           return false
+        )
+      >>= function
+      | true ->
+          return true
+      | false ->
+          logf `Error "Websocket %s didn't respond to ping request"
+            (Slack_api_teamid.to_string x.conn_id);
+          remove_connection x.conn_id >>= fun () ->
+          return false
 
 (*
    Testing:
